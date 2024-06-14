f1e3665822c6c0b28242721987ae3f510aec2118
kirandash <thekirankumardash@gmail.com>
2024-06-14 21:31:51 +0800

    1.8 OpenAI API and playground parameters

--- fundamentals/src/index.ts
+++ fundamentals/src/index.ts
@@ -12,7 +12,7 @@ const openai = new OpenAI({
 const main = async () => {
   // Define the prompt
   const prompt =
-    "I need to start resistance training. Can you create a 7-day workout plan for me to ease into it? Limit it in 30 words or less.";
+    "I need to start resistance training. Can you create a 7-day detailed workout plan for me to ease into it? Limit it in 100 words or less.";
 
   // send api request
   const response = await openai.chat.completions.create({
@@ -28,11 +28,16 @@ const main = async () => {
         content: prompt,
       },
     ],
+    max_tokens: 60,
+    n: 2,
+    frequency_penalty: 1.5,
+    seed: 88888,
   });
 
   // Print the response
   // this will be role "assistant" in the response
   console.log(response.choices[0].message);
+  console.log(response.choices[1].message);
 };
 
 const encodePrompt = (prompt: string) => {
