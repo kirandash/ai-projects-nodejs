9da20cd69a3c7f313b2a84111d2fe27f53f9d6d1
kirandash <thekirankumardash@gmail.com>
2024-06-14 22:22:35 +0800

    2.3 Add chat history (context)

--- chat-app/src/basic/index.ts
+++ chat-app/src/basic/index.ts
@@ -2,21 +2,30 @@ import { OpenAI } from "openai";
 
 const openai = new OpenAI();
 
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+  {
+    role: "system",
+    content: "Act like a cool bro",
+  },
+];
+
+const createChat = async () => {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: context,
+  });
+  const responseMessage = response.choices[0].message;
+  context.push(responseMessage);
+
+  console.log(response.choices[0].message.content);
+};
+
 // process.stdin.addListener is used to read user input from the command line
 process.stdin.addListener("data", async (input) => {
   const userInput = input.toString().trim();
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "Act like a cool bro like: Noor Muhammad.",
-      },
-      {
-        role: "user",
-        content: userInput,
-      },
-    ],
+  context.push({
+    role: "user",
+    content: userInput,
   });
-  console.log(response.choices[0].message.content);
+  await createChat();
 });
