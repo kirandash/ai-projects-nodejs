d0c3a102b8ab554bf9d924dfbd64cff792b6b94a
kirandash <thekirankumardash@gmail.com>
2024-06-14 23:06:29 +0800

    2.4 Set token limit

--- chat-app/src/basic/index.ts
+++ chat-app/src/basic/index.ts
@@ -1,6 +1,10 @@
 import { OpenAI } from "openai";
+import { encoding_for_model } from "tiktoken";
 
 const openai = new OpenAI();
+const encoder = encoding_for_model("gpt-3.5-turbo");
+
+const MAX_TOKENS = 500;
 
 const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
   {
@@ -17,6 +21,10 @@ const createChat = async () => {
   const responseMessage = response.choices[0].message;
   context.push(responseMessage);
 
+  if (response.usage && response.usage.total_tokens > MAX_TOKENS) {
+    removeOlderTokens();
+  }
+
   console.log(response.choices[0].message.content);
 };
 
@@ -29,3 +37,36 @@ process.stdin.addListener("data", async (input) => {
   });
   await createChat();
 });
+
+const getContextLength = () => {
+  let length = 0;
+  context.forEach(
+    (message: OpenAI.Chat.Completions.ChatCompletionMessageParam) => {
+      if (typeof message.content === "string") {
+        length += encoder.encode(message.content).length;
+      } else if (Array.isArray(message.content)) {
+        message.content.forEach((content) => {
+          if (content.type === "text") {
+            length += encoder.encode(content.text).length;
+          }
+        });
+      }
+    }
+  );
+  return length;
+};
+
+const removeOlderTokens = () => {
+  let contextLength = getContextLength();
+  while (contextLength > MAX_TOKENS) {
+    for (let i = 0; i < context.length; i++) {
+      const message = context[i];
+      if (message.role !== "system") {
+        context.splice(i, 1);
+        contextLength = getContextLength();
+        console.log("Updated context length: ", contextLength);
+        break;
+      }
+    }
+  }
+};
