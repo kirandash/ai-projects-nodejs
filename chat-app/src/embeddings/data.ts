bef77a54a47c2714313832a5e3876e76b1947104
kirandash <thekirankumardash@gmail.com>
2024-06-15 18:40:15 +0800

    4.3 Gnerate multiple embeddings and save embeddings json file

--- chat-app/src/embeddings/data.ts
+++ chat-app/src/embeddings/data.ts
@@ -1,4 +1,11 @@
+import { readFileSync, writeFileSync } from "fs";
 import OpenAI from "openai";
+import { join } from "path";
+
+type DataWithEmbeddings = {
+  input: string;
+  embeddings: number[];
+};
 
 const openai = new OpenAI();
 
@@ -10,5 +17,33 @@ const generateEmbeddings = async (input: string | string[]) => {
   console.log(response.data);
   return response;
 };
+
+const loadInputJson = <T>(fileName: string): T => {
+  const path = join(__dirname, fileName);
+  const rawInputData = readFileSync(path);
+  return JSON.parse(rawInputData.toString());
+};
+
+const saveEmbeddingsToJson = (embeddings: any, fileName: string) => {
+  const embeddingsString = JSON.stringify(embeddings);
+  const buffer = Buffer.from(embeddingsString);
+  const path = join(__dirname, fileName);
+  writeFileSync(path, buffer);
+  console.log(`Embeddings saved to ${path}`);
+};
+
+const main = async () => {
+  const input = loadInputJson<string[]>("input.json");
+  const embeddings = await generateEmbeddings(input);
+  const dataWithEmbeddings: DataWithEmbeddings[] = input.map(
+    (input, index) => ({
+      input,
+      embeddings: embeddings.data[index].embedding,
+    })
+  );
+  saveEmbeddingsToJson(dataWithEmbeddings, "dataWithEmbeddings.json");
+};
+
+main();
 
 generateEmbeddings(["Hello", "world"]);
