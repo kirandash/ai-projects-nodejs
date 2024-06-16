96ccb6d8ff61c2420385b0d43d9b7e928f57de7d
kirandash <thekirankumardash@gmail.com>
2024-06-16 19:14:07 +0800

    5.9 Pinecone index operations

--- chat-app/src/pinecone/index.ts
+++ chat-app/src/pinecone/index.ts
@@ -8,7 +8,51 @@ const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY,
 });
 
-const main = async () => {
+type MetaData = {
+  name: string;
+  age: number;
+};
+
+// namespace: partion vector data from an index into multiple namespaces or groups. This will make the operations limited to a specific namespace.
+const createNameSpace = async () => {
+  const index = getIndex("bgwebagency-chat-app-index");
+  const namespace = index.namespace("bgwebagency-chat-app-namespace");
+};
+
+const listIndexes = async () => {
+  const indexes = await pinecone.listIndexes();
+  console.log(indexes);
+};
+
+const getIndex = (indexName: string) => {
+  const index = pinecone.index<MetaData>(indexName);
+  console.log(index);
+  return index;
+};
+
+// Generate random vectors
+const generateRandomVectors = (length: number) => {
+  return Array.from({ length }, () => Math.random());
+};
+
+const upsertVectors = async () => {
+  const embedding = generateRandomVectors(1536);
+  const index = getIndex("bgwebagency-chat-app-index");
+
+  await index.upsert([
+    {
+      id: "id-2",
+      values: embedding,
+      metadata: {
+        name: "John Doe",
+        age: 25,
+      },
+    },
+  ]);
+  console.log("upsert successful");
+};
+
+const createIndex = async () => {
   await pinecone.createIndex({
     name: "bgwebagency-chat-app-index",
     dimension: 1536,
@@ -20,6 +64,24 @@ const main = async () => {
       },
     },
   });
+};
+
+const queryVectors = async () => {
+  const index = getIndex("bgwebagency-chat-app-index");
+  const results = await index.query({
+    id: "id-2",
+    // how many query results to return
+    topK: 1,
+    includeMetadata: true,
+  });
+  console.log(results);
+};
+
+const main = async () => {
+  // await listIndexes();
+  // getIndex("bgwebagency-chat-app-index");
+  // await upsertVectors();
+  await queryVectors();
 };
 
 main();
